Fixed silly reference errors, discovered discrepency between my code and Ganga's in volume calculation. Wrote GI cal test
Former-commit-id: 084f094eb634cb318c396fd9f7e736cf98f249a8

src/training_set.py
@@ -2,6 +2,8 @@
 # This file contains classes and methods that assist in reading and writing
 # training set files in the custom format used for neural network potentials.
 
+import numpy as np
+
 # this parses and stores the header information
 from config import PotentialConfig
 from util   import ProgressBar
@@ -20,18 +22,18 @@ class TrainingSet:
 	def loadFromText(self, text):
 		lines = text.rstrip().split('\n')
 
-		self.config = PotentialConfig('\n'.join(lines[:8]))
+		self.config = PotentialConfig().loadFromText('\n'.join(lines[:8]))
 
-		self.potential_type = int(self._getCellsFromLine(self.lines[8])[1])
-		self.n_structures   = int(self._getCellsFromLine(self.lines[9])[1])
-		self.n_atoms        = int(self._getCellsFromLine(self.lines[10])[1])
+		self.potential_type = int(self._getCellsFromLine(lines[8])[0])
+		self.n_structures   = int(self._getCellsFromLine(lines[9])[0])
+		self.n_atoms        = int(self._getCellsFromLine(lines[10])[0])
 
 		parameters_per_atom  = self.config.n_legendre_polynomials
 		parameters_per_atom *= self.config.n_r0
 
 		progress = ProgressBar(
 			"Loading Training Set", 
-			30, self.n_structures, update_every = 10
+			22, self.n_structures, update_every = 10
 		)
 
 		# Every set of two lines from 13 onwards should correspond to a single 
@@ -44,11 +46,11 @@ class TrainingSet:
 		idx             = 12
 		current_struct  = []
 		current_id      = 0
-		while idx < len(self.lines):
+		while idx < len(lines):
 			
 			atom = TrainingInput().fromLines(
-				self.lines[idx], 
-				self.lines[idx + 1], 
+				lines[idx], 
+				lines[idx + 1], 
 				parameters_per_atom
 			)
 
@@ -82,25 +84,26 @@ class TrainingSet:
 		# The PoscarLoader class itself is iterable. It will return a single
 		# structure per iteration, each structure being an instance of
 		# poscar.py -> PoscarStructure
-		current_group_id = 0
-		current_group    = poscar.structures[0][0].comment
+		current_group_id = 1
+		current_group    = poscar.structures[0].comment
 		for struct_idx, struct in enumerate(poscar):
 			group_name = struct.comment
 			if group_name != current_group:
 				current_group_id += 1
 				current_group     = group_name
 
+			# This is divided by two to match previous code.
 			struct_volume = np.linalg.norm(
 				np.dot(
 					np.cross(struct.a1, struct.a2),
 					struct.a3
 				)
-			)
+			) / 2
 
 			training_input = TrainingInput()
 			training_input.group_name        = group_name
 			training_input.group_id          = current_group_id
-			training_input.structure_id      = struct_idx
+			training_input.structure_id      = struct_idx 
 			training_input.structure_n_atoms = struct.n_atoms
 			training_input.structure_energy  = struct.energy
 			training_input.structure_volume  = struct_volume
@@ -113,13 +116,15 @@ class TrainingSet:
 
 			self.structures.append(current_struct)
 
+		return self
+
 	# Writes the current instance to the specified file. Will overwrite any 
 	# file that is already at that path.
 	def writeToFile(self, file_path):
 		# 50 Kb buffer because these files are always large. This should
 		# make the write a little faster.
 		with open(file_path, 'w', 1024*50) as file:
-			file.write(config.toFileString(prepend_comment=True))
+			file.write(self.config.toFileString(prepend_comment=True))
 			file.write(' # %i - Potential Type\n'%(1))
 			file.write(' # %i - Number of Structures\n'%(self.n_structures))
 			file.write(' # %i - Number of Atoms\n'%(self.n_atoms))
@@ -128,7 +133,7 @@ class TrainingSet:
 
 			progress = ProgressBar(
 				"Writing LSParams ", 
-				30, self.n_atoms, update_every = 50
+				22, self.n_atoms, update_every = 50
 			)
 
 			atom_idx = 0
@@ -210,6 +215,7 @@ class TrainingInput:
 			self.structure_params[idx] = float(val)
 
 		return self
+
 
 	# Extracts all of the relevent cells of information from a line, split
 	# on ' ' characters. Also removes '#' characters.

test/test_lsp.py
@@ -4,5 +4,150 @@
 # in the ref folder. These files were generated using the previous lsp 
 # generating code, written in c. They are assumed to be correct.
 
+import subprocess
+import os
+import numpy as np
+
+# This should remove any complications related to executing the
+# program from this working directory.
+from sys import path
+path.append("../src")
+
+from training_set import TrainingSet
+
+# These are all maximum tolerable error statistics in percent.
+max_err_threshold  = 0.01
+mean_err_threshold = 0.00001
+std_err_threshold  = 0.00001
+
+def compareInputs(new, old, i, j):
+	def location():
+		msg  = "structure: %i, input: %i"
+		msg %= (i, j)
+		return msg
+
+	def err(a, b):
+		print(print(location()))
+		print('%s != %s'%(a, b))
+
+	if new.group_name != old.group_name:
+		err(new.group_name, old.group_name)
+		return False
+
+	if new.group_id != old.group_id:
+		err(new.group_id, old.group_id)
+		return False
+
+	if new.structure_id != old.structure_id:
+		err(new.structure_id, old.structure_id)
+		return False
+
+	if new.structure_n_atoms != old.structure_n_atoms:
+		err(new.structure_n_atoms, old.structure_n_atoms)
+		return False
+
+	if new.structure_energy != old.structure_energy:
+		err(new.structure_energy, old.structure_energy)
+		return False
+
+	if new.structure_volume != old.structure_volume:
+		err(new.structure_volume, old.structure_volume)
+		return False
+
+def compareTrainingSets(new, old):
+	new_set = TrainingSet().loadFromFile(new)
+	old_set = TrainingSet().loadFromFile(old)
+
+	# First, compare all of the structural parameters.
+	for i, struct in enumerate(new_set.structures):
+		for j, new_in in enumerate(struct):
+			# Firstly, make sure that the same exact item exists in 
+			# the old training set.
+			def location():
+				msg  = "structure: %i, input: %i"
+				msg %= (i, j)
+				return msg
+
+			try:
+				old_in = old_set.structures[i][j]
+			except:
+				print(location() + ' missing.')
+				return False
+
+			# Compare all members between the two and summarize differences if
+			# they are found.
+			if not compareInputs(new_in, old_in, i, j):
+				return False
+
+			# Now we get the error in the structure parameters.
+			percent_errors  = new_in.structure_params
+			percent_errors -= old_in.structure_params
+			percent_errors /= old_in.structure_params
+			percent_errors  = np.abs(percent_errors) * 100
+
+			_max = percent_errors.max()
+			_avg = percent_errors.mean()
+			_std = percent_errors.std()
+
+			err = False
+			if _max > max_err_threshold:
+				print("structural parameter error exceeded max threshold.")
+				err = True
+
+			if _avg > mean_err_threshold:
+				print("structural parameter error exceeded mean threshold.")
+				err = True
+
+			if _std > std_err_threshold:
+				print("structural parameter error exceeded std threshold.")
+				err = True
+				
+
+			if err:
+				new_str = ' '.join([str(i) for i in new_in.structure_params])
+				print('\tnew: %s'%(new_str))
+
+				old_str = ' '.join([str(i) for i in old_in.structure_params])
+				print('\told: %s'%(old_str))
+
+				return False
+
+
 if __name__ == '__main__':
-	pass
+	lsparam_names = ['out/%02i.lsparam.nogit.dat'%i for i in range(10)]
+	nn_names      = ['nn/%02i.nn.dat'%i   for i in range(10)]
+	ref_names     = ['ref/%02i.lsparam.dat'%i for i in range(10)]
+	set_name      = 'input/ab.poscar.dat'
+
+	# Make sure that none of the output files already exist.
+	for lsp in lsparam_names:
+		if os.path.isfile(lsp):
+			print('%s already exists, deleting . . . '%lsp)
+			os.remove(lsp)
+
+	# For each file in the list of lsparam_names, run pyfit, set to
+	# generate an lsparam file. After creating each file, load the
+	# corresponding ref file and compare it. Do the comparision before
+	# running the next generation script, in order to catch errors 
+	# before wasting too much time.
+
+	for lsp, ref, nn in zip(lsparam_names, ref_names, nn_names):
+		run_str  = 'python3 ../src/pyfit.py --config-file=test_config.json '
+		run_str += '--generate-training-set --dft-file=%s '
+		run_str += '--training-set-out=%s '
+		run_str += '--network-input-file=%s'
+		run_str %= (set_name, lsp, nn)
+
+		print(run_str)
+		output = subprocess.getoutput(run_str)
+
+		if os.path.isfile(lsp):
+			# Load the file and compare it to the ref file.
+			if not compareTrainingSets(lsp, ref):
+				print("Comparison failed, exiting . . . ")
+				break
+		else:
+			print("pyfit failed to write the lsparam file")
+			print("stdout:\n\n")
+			print(output)
+			break