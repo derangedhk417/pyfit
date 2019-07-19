d2116c0c1349049b63d4720749bc04c5437626e4
Adam Robinson <derangedhk417@gmail.com>
2019-07-19 13:09:27 -0400

    Improved install script
    Former-commit-id: dd42db17366451f5079eb92cb4cf0f8e2f9f0276

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export PATH="$PATH:$PWD"
-echo "export PATH=\"\$PATH:$PWD\"" >> ~/.bashrc
+export PATH="$PATH:$PWD/src"
+echo "export PATH=\"\$PATH:$PWD/src\"" >> ~/.bashrc
+chmod +x $PWD/src/pyfit.py
 echo "Remove the export line referencing $PWD from your .bashrc to reverse the installation procedure."
 echo "Done"
